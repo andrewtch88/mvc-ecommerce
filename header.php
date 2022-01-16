f7c24a31a611a24c798109eacccebfb84234a5cb
andrewtch88 <andrewtch91@gmail.com>
2022-01-16 23:55:51 +0800

    added css framework and navbar

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <script src="./static/materialize/js/materialize.js"></script>
+  <link rel="stylesheet" href="./static/css/base.css">
+</head>
+
+<?php 
+// sessions
+
+?>
+<body>
+  <nav style="height: 100px">
+    <div class="nav-wrapper black">
+    <a href="index.php"><img src = "./static/logo.svg" alt="logo" id="logo" class="brand-logo" height="100"/></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+</body>
+</html>
