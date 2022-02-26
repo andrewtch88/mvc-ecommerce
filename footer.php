c5b783eb3555633745009f1763d248b76823c24e
Zhen-Zhi <tp060844@mail.apu.edu.my>
2022-02-27 01:07:00 +0800

    warranty page and warranty page hyperlink at footer
    Warranty page. unorderlist bullet point not showing

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
     <div class="col s3">
       <h4 class="white-text bold">OG Tech PC</h4>
       <p class="grey-text text-lighten-4">Your favorite online PC store.</p>
+	  <button onclick="window.location.href='warranty page.php';">Warranty page</button>
     </div>
     <div class="col s2">
       <h5 class="white-text">Business Hours</h5>
---
+++ warranty page.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Warranty Info - OG Tech PC</title>
+<?php include "header.php";?>
+</head>
+
+<body>
+
+</body>
+
+<?php include "footer.php";?>
+</html>
