08bbf072fe97296d1f0c03a607e61083f77af1f6
Soongjy <jiayangsoong@GMAIL.COM>
2022-02-27 18:34:45 +0800

    Update page about us

---
+++ aboutus.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>OG Tech - About Us</title>
+  <?php include "header.php"; ?>
+  <style>
+
+  </style>
+</head>
+<body>
+  <h1 class="page-title grid background-overlay" style="margin:1px;padding-top: 140px;padding-bottom: 140px;">About Us</h1>
+  <div class="grid" style="flex-direction:column">
+    <div style="margin:0px;"><h2 class="white-text bold" style="padding-bottom:5px;border-bottom:3px solid red;">About OG Tech</h2></div>
+    <div class="grey-text text-lighten-4">
+      <p style="margin:0px 150px 0px 150px;font-size:25px;text-align:justify">
+        OG Tech was a Company that selling Computer Component which found by four university student and started its business at Malaysia on 1th January 2022.
+        Our passion towards computers makes us willing to introduce and provide the most advanced and recommeded products to our customers.
+        Besides selling Computer Component, our staff will also provide build guides for those who was new to build their PC. 
+        We will recommend and help you build your perfect PC according to your budget.
+        OG tech also continuously update prices from dozens of the most popular online retailers to help you to save your money.
+      </p>
+    </div>
+    <div style="margin:0px;"><h2 class="white-text bold" style="padding-bottom:5px;border-bottom:3px solid red;">History of OG Tech</h2></div>
+    <div class="grey-text text-lighten-4">
+      <p style="margin:0px 150px 0px 150px;font-size:25px;text-align:justify">
+      Early, OG Tech was only a manual store without website and database system, due to the pandamic, the sales had been affected significantly. 
+      Besides, records the information manually using file system was been elaminated in this advanced age. 
+      Therefore, OG Tech Company completely modernize the system from manual system to online system which is a web application system. 
+      However, OG Tech also reserved the old method of selling, which is selling at store, so the customers have two choices of methods to purchase our product. 
+      </p>
+    </div>
+  </div>
+  <?php require "footer.php"; ?>
+</body>
+</html>
