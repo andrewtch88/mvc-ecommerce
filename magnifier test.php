b1c771f124a5836eb3e5e6e53e5fbfb2c533354b
Zhen-Zhi <tp060844@mail.apu.edu.my>
2022-03-03 15:20:10 +0800

    magnifier test

---
+++ magnifier test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="magnifier.css">
+</head>
+
+<body>
+<div>
+    <a class="magnifier-thumb-wrapper">
+        <img src="static/images/aboutusbgimg">
+    </a>
+    <div class="magnifier-preview" id="preview" style="width: 300px; height: 300px">Starry Night Over The Rhone<br>by Vincent van Gogh</div>
+</div>
+</body>
+
+</html>
+
