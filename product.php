400e0a265b5b5424e716823cc6928108eb6f9402
andrewtch88 <andrewtch91@gmail.com>
2022-03-06 00:46:50 +0800

    organized magnifier files

--- product.php
+++ product.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="../HotsTech/static/css/magnifier.css">
+  <link rel="stylesheet" type="text/css" href="static/css/magnifier.css">
   <?php 
     include "header.php";
     require_once "includes/class_autoloader.php";
@@ -236,8 +236,8 @@
 </script>
 
 <script src="static/js/product_page.js"></script>
-<script type="text/javascript" src="../HotsTech/static/js/Event.js"></script>
-<script type="text/javascript" src="../HotsTech/static/js/Magnifier.js"></script>
+<script type="text/javascript" src="static/js/Event.js"></script>
+<script type="text/javascript" src="static/js/Magnifier.js"></script>
 
 <?php include "footer.php"; ?>
 </html>
